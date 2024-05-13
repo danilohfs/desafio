bc6f3614d61d8434442713527f3bcfbb5b0b2fd3
danilohfs <99664726+danilohfs@users.noreply.github.com>
2024-05-13 16:40:42 -0300

    fix(TransferenciaApplication.java), add o enable Hystryx

--- src/main/java/br/com/desafio/TransferenciaApplication.java
+++ src/main/java/br/com/desafio/TransferenciaApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableHystrix
 public class TransferenciaApplication {
     public static void main(String[] args) {
         SpringApplication.run(TransferenciaApplication.class, args);
